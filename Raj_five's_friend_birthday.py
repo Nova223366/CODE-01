7cf6f53583474980e283fb3208533a4e9fcdf640
Nova223366 <jaiaditya39@gmail.com>
2025-09-12 14:36:19 +0530

    Raj's_five_friend_birthday

---
+++ Raj_five's_friend_birthday.py
@@ -0,0 +1,9 @@
+print("Raj's five friend's birthday")
+dob = {
+    "Rohil": "12th Jan",
+    "Aman": "23rd Feb",
+    "Rohin": "5th March",
+    "saqib": "17th Aug",
+    "Ayan": "30th Dec"
+}
+print(dob)

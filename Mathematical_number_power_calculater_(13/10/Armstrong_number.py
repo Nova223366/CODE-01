2424bff0f47742076b592655f3a92b1f2836bd69
Nova223366 <jaiaditya39@gmail.com>
2025-10-16 21:47:27 +0530

    Class_activity_HW

--- Mathematical_number_power_calculater_(13/10/Armstrong_number.py
+++ Mathematical_number_power_calculater_(13/10/Armstrong_number.py
@@ -1,10 +1,8 @@
-num = int(input("Enter your number: "))
-temp = num
-sum = 0
-
-while temp > 0:
-    digit = temp % 10
-    sum += digit ** 3
-    temp //= 10
-
-print("Sum of cubes of digits:", sum)
+string = input("Enter your word: ")
+character = input("Enter a word you want to: ")
+count = 0
+while string.__len__() > count:
+    if string[count] == character:
+        print(f"Character '{character}' found at index {count}")
+    count += 1
+print(f"Total occurrences of '{character}': {string.count(character)}")

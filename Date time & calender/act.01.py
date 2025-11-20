cdeef528a1114b1b7cad7e80b274f18c9dd9a879
jaiaditya39@gmail.com <jaiaditya39@gmail.com>
2025-11-20 22:27:15 +0530

    Rina calenderd

--- Date time & calender/act.01.py
+++ Date time & calender/act.01.py
@@ -2,7 +2,8 @@ from datetime import date, time ,datetime
 
 today = date.today()
 now = datetime.now()
-print("Today's date:", today)
-print("\nCurrent date and time:", now)
+print("Hey rina, may your codeing journey be filled with joy and success! Happy coding!")
+print("\n\tToday's date:", today)
+print("\tCurrent date and time:", now)
 
-print("Current Year:", today.year, today.month, today.day)
+print("\nCurrent Year:", today.day, today.month, today.year)
